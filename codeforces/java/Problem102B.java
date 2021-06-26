4ef6f9bfbffaf71e9864f5f89a91e5679a7054c1
Phan Truong Thanh <thanh@quod.ai>
2021-06-26 10:49:27 +0700

    Update Problem102B.java

--- codeforces/java/Problem102B.java
+++ codeforces/java/Problem102B.java
@@ -3,36 +3,35 @@ import java.util.Scanner;
 
 public class Problem102B {
 
-    private static void readAndSolve() {
-        Scanner in = new Scanner(System.in);
-        BigInteger input = in.nextBigInteger();
-        System.out.println(solve(input));
-    }
+  private static void readAndSolve() {
+    final Scanner in = new Scanner(System.in);
+    final BigInteger input = in.nextBigInteger();
+    System.out.println(solve(input));
+  }
 
-    private static int solve(BigInteger input) {
-        String s = input.toString();
-        if (s.length() == 1) return 0;
-        int total = 0;
-        int castSpellTime = 1;
-        for (var i : s.toCharArray()) {
-            int numericValue = Character.getNumericValue(i);
-            total += numericValue;
-            while (total > 9) {
-                total = String.valueOf(total).chars().map(Character::getNumericValue).sum();
-                castSpellTime++;
-            }
-        }
-        return castSpellTime;
+  private static int solve(final BigInteger input) {
+    final String s = input.toString();
+    if (s.length() == 1) return 0;
+    int total = 0;
+    int castSpellTime = 1;
+    for (final var i : s.toCharArray()) {
+      final int numericValue = Character.getNumericValue(i);
+      total += numericValue;
+      while (total > 9) {
+        total = total % 10 + 1;
+        castSpellTime++;
+      }
     }
+    return castSpellTime;
+  }
 
-    private static void test() {
-        assert solve(BigInteger.valueOf(0)) == 0;
-        assert solve(BigInteger.valueOf(10)) == 1;
-        assert solve(BigInteger.valueOf(991)) == 3;
-        assert solve(BigInteger.valueOf(123456789)) == 2;
-    }
+  private static void test() {
+    assert solve(BigInteger.valueOf(0)) == 0;
+    assert solve(BigInteger.valueOf(10)) == 1;
+    assert solve(BigInteger.valueOf(991)) == 3;
+  }
 
-    public static void main(String[] args) {
-        test();
-    }
+  public static void main(final String[] args) {
+    readAndSolve();
+  }
 }
