fd18b6e87753f67449134e0a786c920946c37a87
Phan Truong Thanh <thanh@quod.ai>
2021-07-28 20:12:26 +0700

    Update Problem732A.java

--- codeforces/java/Problem732A.java
+++ codeforces/java/Problem732A.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 
 public class Problem732A {
 
-  private static int solve(final int k, final int r) {}
+  private static int solve(final int k, final int r) {
+    final String s = String.valueOf(k);
+    final int finalInt = Character.getNumericValue(s.charAt(s.length() - 1));
+    if (finalInt == r) return 1;
+    int currentInt = finalInt;
+    int currentBuy = 1;
+    while (currentInt != r && currentInt != 0) {
+      currentBuy++;
+      currentInt = finalInt * currentBuy;
+      currentInt %= 10;
+    }
+    return currentBuy;
+  }
 
   private static void readAndSolve() {
     final Scanner in = new Scanner(System.in);
